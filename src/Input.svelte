841a3bffe4f964d1e99cdf540d4a955349c394db
monroy95 <m.monroyc22@gmail.com>
2020-04-18 14:39:00 -0600

    Renderizado condicional

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,15 +1,26 @@
 <script>
   let value = "";
+  let loading = false;
+  let response = [];
   const handleInput = () => (value = event.target.value);
 
   $: if (value.length > 2) {
+    loading = true;
     fetch(`http://www.omdbapi.com/?s=${value}&apikey=fd906ccc`)
       .then(res => res.json())
       .then(apiResponse => {
-        let reponse = apiResponse.Search;
-        console.log(reponse);
+        response = apiResponse.Search || [];
+        loading = false;
       });
   }
 </script>
 
-<input type="text" {value} on:input={handleInput} />
+<input placeholder="Search movies..." {value} on:input={handleInput} />
+
+{#if loading > 0}
+  <strong>Buscando ...</strong>
+{:else if response.length > 0}
+  <strong>Tenemos {response.length}</strong>
+{:else}
+  <strong>No hay resultados</strong>
+{/if}
