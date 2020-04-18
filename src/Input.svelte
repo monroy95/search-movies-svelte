37ea2ebabcf2d04eb8426bb0e8ef0ab2c8e85cca
monroy95 <m.monroyc22@gmail.com>
2020-04-18 14:10:24 -0600

    Agregando code para consumir API OMDB

---
+++ src/Input.svelte
@@ -0,0 +1,15 @@
+<script>
+  let value = "";
+  const handleInput = () => (value = event.target.value);
+
+  $: if (value.length > 2) {
+    fetch(`http://www.omdbapi.com/?s=${value}&apikey=fd906ccc`)
+      .then(res => res.json())
+      .then(apiResponse => {
+        let reponse = apiResponse.Search;
+        console.log(reponse);
+      });
+  }
+</script>
+
+<input type="text" {value} on:input={handleInput} />
